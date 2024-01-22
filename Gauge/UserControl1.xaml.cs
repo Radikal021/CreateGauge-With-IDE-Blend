b04d0d6da7f306dbe660d6e152f8587739218c03
Radikal <diyanat00emad@gmail.com>
2024-01-23 02:58:11 +0330

    Add files via upload

---
+++ Gauge/UserControl1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Gauge
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public void SetValuePoint(int Value)
+        {
+            double Mathpoint = Value * 2.6;
+            double pointposition = -130 + Mathpoint;
+
+            Storyboard DailBoard = new Storyboard();
+            DoubleAnimationUsingKeyFrames DA = new DoubleAnimationUsingKeyFrames();
+            EasingDoubleKeyFrame frame = new EasingDoubleKeyFrame();
+
+            Storyboard.SetTarget(DA, arrowPointer);
+            Storyboard.SetTargetProperty(DA, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[2].(RotateTransform.Angle)"));
+
+            frame.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(1));
+            frame.Value = pointposition;
+
+            DA.KeyFrames.Add(frame);
+            DailBoard.Children.Add(DA);
+
+            // Update the value indicator
+            lbl_Value.Content = Value.ToString() + "%";
+
+            // Start the animation
+            DailBoard.Begin();
+        }
+
+
+    }
+}
